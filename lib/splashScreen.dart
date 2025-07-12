5aef45be70b306c5452d5438295ca90e4193a96c
Ranjeet Dethe <130579549+ranjeetdethe@users.noreply.github.com>
2025-07-12 12:50:24 +0530

    Update splashScreen.dart

--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -1,71 +1,32 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
+
+import 'package:flutter/material.dart';
 import 'package:multigenesys_app/view/emp_list.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  _SplashScreenState createState() => _SplashScreenState();
+  State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _scaleAnimation;
-  late Animation<double> _fadeAnimation;
-
+class _SplashScreenState extends State<SplashScreen> {
   @override
-  void initState() {
-    super.initState();
-
-    _controller = AnimationController(
-      duration: const Duration(seconds: 3),
-      vsync: this,
-    );
-
-    _scaleAnimation = Tween<double>(
-      begin: 1.2,
-      end: 1.0,
-    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
-
-    _fadeAnimation = Tween<double>(
-      begin: 0.0,
-      end: 1.0,
-    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
-
-    _controller.forward();
-
-    Timer(const Duration(seconds: 4), () {
-      Navigator.pushReplacement(
-        context,
+  Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => EmployeeListScreen()),
       );
     });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FadeTransition(
-          opacity: _fadeAnimation,
-          child: ScaleTransition(
-            scale: _scaleAnimation,
-            child: Image.asset(
-              'assets/logo1.png',
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
+      body: Column(
+        children: [
+          Spacer(),
+          Center(child: Image.asset("assets/sp_image.jpg")),
+          Spacer(),
+
+          // SizedBox(height: 100),
+        ],
       ),
     );
   }
